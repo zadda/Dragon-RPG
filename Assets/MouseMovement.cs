6fb5a0adea1ca8ae1bb7c10fe63923c9d6eab53d
mike_b <atm89@hotmail.com>
2017-03-19 08:59:29 +0100

    camera follows player

---
+++ Assets/MouseMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour 
+{
+
+    private GameObject player;
+    private Vector3 cursor;
+
+	// Use this for initialization
+	void Start () 
+	{
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            cursor.x = Input.mousePosition.x;
+            OnMuisKlik();
+        }
+	}
+
+    void OnMuisKlik()
+    {
+        player.transform.position = cursor;
+    }
+
+}
